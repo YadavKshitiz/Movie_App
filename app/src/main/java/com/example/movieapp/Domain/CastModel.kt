c6a53791a9659f252438ce78f3bbc4111f8d939a
Kshitz Yadav <ahirkshitiz.pb@gmail.com>
2025-09-24 10:08:03 +0530

    Updated till viewmodel

---
+++ app/src/main/java/com/example/movieapp/Domain/CastModel.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.Domain
+
+import java.io.Serializable
+data class CastModel(
+    var PicUrl:String="",
+    var Actor:String=""
+):Serializable
---
+++ app/src/main/java/com/example/movieapp/ViewModel/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.movieapp.ViewModel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+
+}
+
