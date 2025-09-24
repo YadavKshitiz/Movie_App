b351e87a62abbfe8f23faa4dc5da2e984b6825e2
Kshitz Yadav <ahirkshitiz.pb@gmail.com>
2025-09-24 19:24:32 +0530

    ViewModel file added

--- app/src/main/java/com/example/movieapp/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/movieapp/ViewModel/MainViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.movieapp.ViewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.movieapp.Domain.FilmItemModel
+import com.example.movieapp.Repository.MainRepository
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
+    private val repository = MainRepository()
 
+    fun loadUpcoming(): LiveData<MutableList<FilmItemModel>> {
+        return repository.loadUpcoming()
+
+    }
+
+    fun loaditems(): LiveData<MutableList<FilmItemModel>> {
+        return repository.loadItems()
+    }
 
 }
 
